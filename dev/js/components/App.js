71856cd12ef9c47c2ab965321244dc01fa837dfc
kennethlumalicay <kennethlumalicay@gmail.com>
2017-11-22 05:19:32 -0500

    added github repo

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <Notif/>
         <Routes/>
         <footer>
-          <p>App made by <a href='https://kennethlumalicay.github.io/' target='_blank' rel='noopener noreferrer'>Kenneth Malicay</a>. Don't forget to star the <a href='#' target='_blank' rel='noopener noreferrer'>github repo</a> if you liked this!</p>
+          <p>App made by <a href='https://kennethlumalicay.github.io/' target='_blank' rel='noopener noreferrer'>Kenneth Malicay</a>. Don't forget to star the <a href='https://github.com/kennethlumalicay/betterweb' target='_blank' rel='noopener noreferrer'>github repo</a> if you liked this!</p>
         </footer>
       </section>
     );
