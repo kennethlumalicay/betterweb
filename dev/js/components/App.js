8f3160d806d88e7b31f87329c9737976084cee35
kennethlumalicay <kennethlumalicay@gmail.com>
2017-11-30 13:32:15 -0500

    added betterweb twitter on footer

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <Notif/>
         <Routes/>
         <footer>
-          <p>App made by <a href='https://kennethlumalicay.github.io/' target='_blank' rel='noopener noreferrer'>Kenneth Malicay</a>. Don't forget to star the <a href='https://github.com/kennethlumalicay/betterweb' target='_blank' rel='noopener noreferrer'>github repo</a> if you liked this!</p>
+          <p>App made by <a href='https://kennethlumalicay.github.io/' target='_blank' rel='noopener noreferrer'>Kenneth Malicay</a>. Don't forget to star the <a href='https://github.com/kennethlumalicay/betterweb' target='_blank' rel='noopener noreferrer'>github repo</a> and follow <a href='https://twitter.com/BetterWebTech' target='_blank' rel='noopener noreferrer'>@BetterWebTech</a> on twitter if you liked this!</p>
         </footer>
       </section>
     );
