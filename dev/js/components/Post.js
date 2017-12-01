14c70c2f4a764f1bf11d057bb0009e5c0cf82d18
kennethlumalicay <kennethlumalicay@gmail.com>
2017-12-01 10:59:22 -0500

    post layout change

--- dev/js/components/Post.js
+++ dev/js/components/Post.js
@@ -14,25 +14,29 @@ export default (props) => (
         </div>
       )
       : null }
-    <div className='post-img' style={{backgroundImage: 'url('+props.post.imgLocation+')'}}></div>
-    <h2>{props.post.title}</h2>
-    <p>{props.post.description}</p>
-    <div className='post-links'>
-      { props.post.liveLink
-        ? <a href={props.post.liveLink} target='_blank' rel='noopener noreferrer'><i className='fa fa-globe' aria-hidden='true'></i></a>
-        : null }
-      { props.post.githubLink
-        ? <a href={props.post.githubLink} target='_blank' rel='noopener noreferrer'><i className='fa fa-github' aria-hidden='true'></i></a>
-        : null }
-      { !props.page
-        ? <Link to={'/post/' + props.post.pid}><i className='fa fa-commenting' aria-hidden='true'></i> <sup>{props.post.commentCount || ''}</sup></Link>
-        : null }
+    <Link to={'/post/' + props.post.pid}>
+      <div className='post-img' style={{backgroundImage: 'url('+props.post.imgLocation+')'}}></div>
+    </Link>
+    <div className='post-content'>
+      <h2>{props.post.title}</h2>
+      <p>{props.post.description}</p>
+      <div className='post-links'>
+        { props.post.liveLink
+          ? <a href={props.post.liveLink} target='_blank' rel='noopener noreferrer'><i className='fa fa-globe' aria-hidden='true'></i></a>
+          : null }
+        { props.post.githubLink
+          ? <a href={props.post.githubLink} target='_blank' rel='noopener noreferrer'><i className='fa fa-github' aria-hidden='true'></i></a>
+          : null }
+        { !props.page
+          ? <Link to={'/post/' + props.post.pid}><i className='fa fa-commenting' aria-hidden='true'></i> <sup>{props.post.commentCount || ''}</sup></Link>
+          : null }
+      </div>
+      <h3>
+        { props.post.guest
+          ? props.post.username
+          : <Link to={'/user/' + props.post.uid}>{props.post.username}</Link> }
+        {oneTag(props.post.usertag, true)}
+      </h3>
     </div>
-    <h3>
-      { props.post.guest
-        ? props.post.username
-        : <Link to={'/user/' + props.post.uid}>{props.post.username}</Link> }
-      {oneTag(props.post.usertag, true)}
-    </h3>
   </div>
 );
