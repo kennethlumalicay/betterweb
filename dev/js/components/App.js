4224f8ac58ceeefdf24ea659a7c8ab60c714f65e
kennethlumalicay <kennethlumalicay@gmail.com>
2017-12-09 00:18:04 -0500

    updated footer

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         <Notif/>
         <Routes/>
         <footer>
-          <p>App made by <a href='https://kennethlumalicay.github.io/' target='_blank' rel='noopener noreferrer'>Kenneth Malicay</a>. Don't forget to star the <a href='https://github.com/kennethlumalicay/betterweb' target='_blank' rel='noopener noreferrer'>github repo</a> and follow <a href='https://twitter.com/BetterWebTech' target='_blank' rel='noopener noreferrer'>@BetterWebTech</a> on twitter if you liked this!</p>
+          <p>App made by <a href='https://kennethlumalicay.github.io/' target='_blank' rel='noopener noreferrer'>Kenneth Malicay</a>. Don't forget to star and fork the <a href='https://github.com/kennethlumalicay/betterweb' target='_blank' rel='noopener noreferrer'>github repo</a>!</p>
+          <p>Follow <a href='https://twitter.com/BetterWebTech' target='_blank' rel='noopener noreferrer'>@BetterWebTech</a> on <a href='https://twitter.com/BetterWebTech' target='_blank' rel='noopener noreferrer'>twitter</a> and <a href='https://www.facebook.com/BetterWebTech/' target='_blank' rel='noopener noreferrer'>facebook</a> if you liked this!</p>
         </footer>
       </section>
     );
