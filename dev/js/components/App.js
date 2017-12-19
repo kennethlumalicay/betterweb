d16faa938b1821e965059f936b30829074994db1
kennethlumalicay <kennethlumalicay@gmail.com>
2017-12-19 15:01:23 -0500

    quote change and donate value

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <Routes/>
         <footer>
           <p>App made by <a href='https://kennethlumalicay.github.io/' target='_blank' rel='noopener noreferrer'>Kenneth Malicay</a>. Don't forget to star and fork the <a href='https://github.com/kennethlumalicay/betterweb' target='_blank' rel='noopener noreferrer'>github repo</a>!</p>
-          <p><a onClick={() => logOutboundLink('paypal')} href='https://paypal.me/kennethlumalicay/3' target='_blank' rel='noopener noreferrer'>Buy me a coffee or help keep the server up and running.</a></p>
+          <p><a onClick={() => logOutboundLink('paypal')} href='https://paypal.me/kennethlumalicay/2' target='_blank' rel='noopener noreferrer'>Buy me a coffee or help keep the server up and running.</a></p>
         </footer>
       </section>
     );
--- dev/js/components/Home.js
+++ dev/js/components/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
         <div className='splash'>
           <div>
             <h1>Giving each other feedback should be a culture</h1>
-            <p>Both getting and giving feedback are learning opportunities, use it to help create a betterweb.</p>
+            <p>Let's make a feedback culture in web development so we can all grow and learn from one another.</p>
             <div className='splash-social'>
               <a href='https://www.facebook.com/BetterWebTech/' target='_blank' rel='noopener noreferrer'><i className='fa fa-facebook' aria-hidden='true'></i></a> 
               <a href='https://twitter.com/BetterWebTech' target='_blank' rel='noopener noreferrer'><i className='fa fa-twitter' aria-hidden='true'></i></a>
